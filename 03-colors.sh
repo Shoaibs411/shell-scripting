cb1e32a9337c29069bacbbb98324c7aa1793394e
Shoaib <shoaibs411@gmail.com>
2023-12-02 19:40:45 +0530

    Auto Push

---
+++ 03-colors.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Each and every color you see on terminal will have a color code and we need to use that code baesd on our need.
+
+# Colors       Foreground          Background
+# Red               31                  41
+# Green             32                  42
+# Yellow            33                  43
+# Blue              34                  44
+# Magenta           35                  45
+# Cyan              36                  46
+
+
+# Syntax to Print COLORS is : 
+
+# printing the color 
+# echo -e "\e[COLORCODEm I am printing Red Color \e [0m"
+echo "\n"
+echo -e "\e[31m I am printing Red Color \e [0m"
+echo -e "\e[32m I am printing Red Color \e [0m"
+echo -e "\e[33m I am printing Red Color \e [0m"
+echo -e "\e[34m I am printing Red Color \e [0m"
+echo -e "\e[35m I am printing Red Color \e [0m"
+echo -e "\e[36m I am printing Red Color \e [0m"
