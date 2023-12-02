d1313380ec6f4e5b93d69fad332cf02ff0678d3f
Shoaib <shoaibs411@gmail.com>
2023-12-02 19:35:42 +0530

    updated shebang.sh

--- 02-esc-sequence.sh
+++ 02-esc-sequence.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-echo Welcome to shell scripting.
-echo "This is my 1st day of shell scripting"
+echo Welcome To Shell Scripting
+echo "This is day 1 of our shell scripting"
 
-echo -e "\t" "line 1\n \tline 2"  
-echo -e line 3
-echo -e line 4 
+# Escape Sequence characters
+
+#  \n : New Line 
+# \t : tab Space 
+
+echo -e "Line1\nLine2"
+echo -e "Line3\tLine4"
+echo Line5
+
+# Whenever you're using special characters , always enclose them in DOUBLE QUOTES
+# " "  : Double Quotes 
+# ' '  : Single Quotes
