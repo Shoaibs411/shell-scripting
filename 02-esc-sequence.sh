48373f13722553e75470a50c79c70c788ed3ad27
Shoaib <shoaibs411@gmail.com>
2023-12-02 19:33:24 +0530

    updated shebang.sh

--- 02-esc-sequence.sh
+++ 02-esc-sequence.sh
@@ -3,6 +3,6 @@
 echo Welcome to shell scripting.
 echo "This is my 1st day of shell scripting"
 
-echo -e "\t" "line 1\nline 2"  
+echo -e "\t" "line 1\n \tline 2"  
 echo -e line 3
 echo -e line 4 
