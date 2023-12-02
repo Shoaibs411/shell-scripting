5e2ff9a813c2d05487a8eadda2bb745fb1ba5d19
Shoaib <shoaibs411@gmail.com>
2023-12-02 19:43:07 +0530

    Auto Push

--- 03-colors.sh
+++ 03-colors.sh
@@ -22,3 +22,5 @@ echo -e "\e[33m I am printing Red Color \e[0m"
 echo -e "\e[34m I am printing Red Color \e[0m"
 echo -e "\e[35m I am printing Red Color \e[0m"
 echo -e "\e[36m I am printing Red Color \e[0m"
+
+echo -e "\e[43;44m I am printing background + foreground \e[0m"
