9e989af3b65c55e55e5f68ff74d22d6f3c1ace9f
Shoaib <shoaibs411@gmail.com>
2023-12-05 00:53:13 +0530

    Auto Push

--- 15-ifelse.sh
+++ 15-ifelse.sh
@@ -27,4 +27,8 @@ if [ "$ACTION" == "restart" ] ; then
 fi
 
         echo -e "\n \e[31m \033[1m \033[3m \t It has not met any conditions \033[0m \033[0m \e[0m"
+        echo -e "\n \e[31m \033[1m \033[3m \t please supply values in command line as follows \033[0m \033[0m \e[0m"
+        echo -e "\n\t 1.Start \n\t 2.stop \n\t 3.restart"
+        echo -e "\n \e[32m \033[1m \t Example usage :- 'bash scriptname.sh start' \033[0m \e[0m" 
+
         echo -e "\n|--------------------------------------------------------------------|"
