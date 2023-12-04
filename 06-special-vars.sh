fb713fc950c4c746ac5435c3aa065254c1a789e9
Shoaib <shoaibs411@gmail.com>
2023-12-04 19:21:23 +0530

    Auto Push

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -5,14 +5,18 @@ echo -e "\n \e[33m Exploring Special variables in Shell Scripting \e[0m"
 echo -e "\nPrinting the Script Name : \e[34m $0 \e[0m"
 echo -e "\nPrinting the 1st line Argument : \e[34m $1 \e[0m"
 echo -e "\nPrinting the 2nd line Argument : \e[34m $2 \e[0m"
-echo -e "\nPrinting the 3rd line Argument : \e[34m $3 \e[0m"
-echo -e "\nPrinting the 4th line Argument : \e[34m $4 \e[0m"
-echo -e "\nPrinting the 5th line Argument  : \e[34m $5 \e[0m"
-echo -e "\nPrinting the 6th line Argument : \e[34m $6 \e[0m"
-echo -e "\nPrinting the 7th line Argument7' : \e[34m $7 \e[0m"
-echo -e "\nPrinting the 8th line Argument' : \e[34m $8 \e[0m"
-echo -e "\nPrinting the 9th line Argument' : \e[34m $9 \e[0m"
+
+
 
 echo -e "\nPrinting Script Name : \e[34m $0 \e[0m"
 echo -e "\nPrinting all arguments used in script Name : \e[34m $# \e[0m"
-echo -e "\nPrinting exit code of the previous command : \e[34m "$?" \e[0m"
+echo -e "\nPrinting exit code of the previous command : \e[34m $? \e[0m"
+
+echo $0   # Prints Script Name 
+echo $#   # Prints the overall arguments used in the script 
+echo $?   # This prints the exit code of the last command
+echo $*   # Prints all the arguments used.
+echo $@   # Prints all the arguments used.
+
+# echo "Variables Used In The Script $*"    # $* is going to print the used variables  
+# echo "Variabels used are $@"              # $@ is going to print the used variables  
