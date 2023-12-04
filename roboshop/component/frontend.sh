21c5eaa968dbfabe2a952cc5ef9444050cb27271
Shoaib <shoaibs411@gmail.com>
2023-12-05 02:57:27 +0530

    Auto Push

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 USERID=$(id -u)
+COMPONENT=$1
 
 if [ $UID -ne 0 ]; then
     echo -e "\n\e[31m \033[1m This script is expected to be executed with sudo or as a root user \033[0m \e[0m"
@@ -7,7 +8,7 @@ if [ $UID -ne 0 ]; then
     exit 1
 fi
 echo -e "\n|--------------------------------------------------------------|"
-echo -e "\n\t ********** \e[35m \033[1m Configuring frontend \033[0m \e[0m **********"
+echo -e "\n\t ********** \e[35m \033[1m Configuring Frontend \033[0m \e[0m **********"
 
 echo -e -n "\n Installing nginx :"
 
@@ -16,14 +17,19 @@ if [ $? -eq 0 ]; then
     echo -e "\e[32m Successfully Installed \e[0m"
 else    
     echo -e "\e[31m Installation Failed \e[0m"
-
 fi
 
+echo -e -n "\n Downloading the component $1 :"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Successfully Installed \e[0m"
+else    
+    echo -e "\e[31m Installation Failed \e[0m"
+fi
 
 # systemctl enable nginx
 # systemctl start nginx
 
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 # cd /usr/share/nginx/html
 # rm -rf *
 # unzip /tmp/frontend.zip
