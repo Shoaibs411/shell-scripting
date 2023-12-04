138b2f506fcd61f4f0540258505ab4be6880ca6e
Shoaib <shoaibs411@gmail.com>
2023-12-05 00:30:00 +0530

    Auto Push

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -30,7 +30,7 @@ case $ACTION in
     *)
         echo -e "\n \e[31m \033[1m \033[3m \t Valid options are as follows :-\033[0m \033[0m \e[0m"        
         echo -e "\n\t 1.Start \n\t 2.stop \n\t 3.restart"
-        echo -e "\n \e[32m \033[1m \033[3m \t Example usage :- 'bash scriptname.sh start' \033[0m \033[0m \e[0m"        
+        echo -e "\n \e[32m \033[1m \t Example usage :- 'bash scriptname.sh start' \033[0m \e[0m"        
 
         echo -e "\n|--------------------------------------------------------------------|"
 
