a61afb9f9e48d9e34389da9ee243aa7cfb4c9efb
Shoaib <shoaibs411@gmail.com>
2023-12-05 03:01:53 +0530

    Auto Push

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -20,7 +20,7 @@ else
 fi
 
 echo -e -n "\n Downloading the component $1:"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+wget -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
 else    
