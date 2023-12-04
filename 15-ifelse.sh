dbcc75548a7905f4b17df3f728f88a28fa087397
Shoaib <shoaibs411@gmail.com>
2023-12-05 00:54:01 +0530

    Auto Push

--- 15-ifelse.sh
+++ 15-ifelse.sh
@@ -21,13 +21,13 @@ if [ "$ACTION" == "stop" ] ; then
 fi
 
 if [ "$ACTION" == "restart" ] ; then
-        echo -e "\n \e[33m \033[1m \033[3m \t Restarting Shipping Services\033[0m \033[0m \e[0m"
+        echo -e "\n \e[33m \033[1m \t Restarting Shipping Services \033[0m \e[0m"
         echo -e "\n|--------------------------------------------------------------------|"
         exit 2
 fi
 
-        echo -e "\n \e[31m \033[1m \033[3m \t It has not met any conditions \033[0m \033[0m \e[0m"
-        echo -e "\n \e[31m \033[1m \033[3m \t please supply values in command line as follows \033[0m \033[0m \e[0m"
+        echo -e "\n \e[31m \033[1m \t It has not met any conditions \033[0m \e[0m"
+        echo -e "\n \e[31m \033[1m \t please supply values in command line as follows \033[0m \e[0m"
         echo -e "\n\t 1.Start \n\t 2.stop \n\t 3.restart"
         echo -e "\n \e[32m \033[1m \t Example usage :- 'bash scriptname.sh start' \033[0m \e[0m" 
 
