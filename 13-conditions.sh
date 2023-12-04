4b18f05b8cb16c3f6f1454d2623cef52d7feb687
Shoaib <shoaibs411@gmail.com>
2023-12-05 00:24:42 +0530

    Auto Push

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -9,27 +9,27 @@ ACTION=$1
 
 case $ACTION in
     start)
-        echo -e "\n\t \e[35m \033[1m \033[3m Starting Shipping Services\033[0m \033[0m \e[0m"
+        echo -e "\n\t \e[32m \033[1m \033[3m Starting Shipping Services\033[0m \033[0m \e[0m"
         echo -e "\n|--------------------------------------------------------------------|"
 
         exit 0
         ;;
 
     stop)
-        echo -e "\n\t \e[35m \033[1m \033[3m Stopping Shipping Services\033[0m \033[0m \e[0m"        
+        echo -e "\n\t \e[33m \033[1m \033[3m Stopping Shipping Services\033[0m \033[0m \e[0m"        
         echo -e "\n|--------------------------------------------------------------------|"
 
         exit 1
         ;;
     restart)
-        echo -e "\n\t \e[35m \033[1m \033[3m Restarting Shipping Services\033[0m \033[0m \e[0m"        
+        echo -e "\n\t \e[34m \033[1m \033[3m Restarting Shipping Services\033[0m \033[0m \e[0m"        
         echo -e "\n|--------------------------------------------------------------------|"
 
         exit 1
         ;;
     *)
-        echo -e "\n\t \e[35m \033[1m \033[3m Valid options are as follows :-\033[0m \033[0m \e[0m"        
-        echo -e "\n\t 1.Start \n\t 2.stop \n\t 3.restart"
+        echo -e "\n\t \e[31m \033[1m \033[3m Valid options are as follows :-\033[0m \033[0m \e[0m"        
+        echo -e "\n\t       1.Start \n\t 2.stop \n\t 3.restart"
         echo -e "\n|--------------------------------------------------------------------|"
 
         exit 2
