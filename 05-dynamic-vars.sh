fa7fe25b88248aac66a4d0cf9cf47822e4b072db
Shoaib <shoaibs411@gmail.com>
2023-12-04 19:03:21 +0530

    Auto Push

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -6,5 +6,8 @@
 #DATE=21/05/2023
 
 DATE=$(date +%F)
-
+no_of_sessions=$(who| wc -l)
 echo -e "\n \e[32m Good Morning, Today's date is $DATE \e[0m"
+
+
+echo -e "\n \e[32m number of active users in system are $no_of_sessions \e[0m"
