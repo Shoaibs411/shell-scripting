c18f2a05db1f0accc28aa54887fd545371c73e16
Shoaib <shoaibs411@gmail.com>
2023-12-05 00:38:32 +0530

    Auto Push

--- 15-ifelse.sh
+++ 15-ifelse.sh
@@ -4,5 +4,5 @@
 
 
 echo -e "\n|--------------------------------------------------------------------|"
-echo -e "\n \e[35m \033[1m \t Exploring if-else in Shell Scripting \033[0m \e[0m"
+echo -e "\n \e[35m \033[1m \t Exploring If, If Else & Else If in Shell Scripting \033[0m \e[0m"
 echo -e "\n|--------------------------------------------------------------------|"
