1191b9c33441ef6887ddd93767e0823c964085e2
Shoaib <shoaibs411@gmail.com>
2023-12-04 19:25:20 +0530

    Auto Push

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -9,8 +9,10 @@ echo -e "\nPrinting the 2nd line Argument : \e[34m $2 \e[0m"
 
 
 echo -e "\n" '$0' "Prints Script Name : \e[34m $0 \e[0m"
-echo -e "\nPrinting all arguments used in script Name : \e[34m $# \e[0m"
-echo -e "\nPrinting exit code of the previous command : \e[34m $? \e[0m"
+echo -e "\n" '$#' "Prints the overall arguments used in the script : \e[34m $0 \e[0m"
+echo -e "\n" '$?' "prints the exit code of the last command : \e[34m $0 \e[0m"
+echo -e "\n" '$*' "Prints all the arguments used. : \e[34m $0 \e[0m"
+echo -e "\n" '$@' "Prints all the arguments used. : \e[34m $0 \e[0m"
 
 echo $0   # Prints Script Name 
 echo $#   # Prints the overall arguments used in the script 
