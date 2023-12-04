2b354311f5d8efad6d8607d2bb6aff5d1ae297fd
Shoaib <shoaibs411@gmail.com>
2023-12-05 00:19:43 +0530

    Auto Push

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -10,20 +10,28 @@ ACTION=$1
 case $ACTION in
     start)
         echo -e "\n\te[33m \033[1m \033[3mStarting Shipping Services\033[0m \033[0m \e[0m"
+        echo -e "\n|--------------------------------------------------------------------|"
+
         exit 0
         ;;
 
     stop)
         echo -e "\n\te[31m \033[1m \033[3mStopping Shipping Services\033[0m \033[0m \e[0m"
+        echo -e "\n|--------------------------------------------------------------------|"
+
         exit 1
         ;;
     restart)
         echo -e "\n\te[32m \033[1m \033[3mStopping Shipping Services\033[0m \033[0m \e[0m"
+        echo -e "\n|--------------------------------------------------------------------|"
+
         exit 1
         ;;
     *)
         echo -e "\n\te[32m \033[1m \033[3mValid options are as follows :-s\033[0m \033[0m \e[0m"
         echo -e "\n\t 1.Start \n\t 2. stop \n\t 3.restart"
+        echo -e "\n|--------------------------------------------------------------------|"
+
         exit 2
         ;;
 
