df824606e4e88f0c9ef0baa0af8680a1a494b796
Shoaib <shoaibs411@gmail.com>
2023-12-05 01:14:41 +0530

    Auto Push

---
+++ 16-loops.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exploring Loops in Shell scripting.
+
+
+echo -e "\n|--------------------------------------------------------------------|"
+echo -e "\n \e[35m \033[1m \t Exploring Loops in Shell Scripting \033[0m \e[0m"
+
+# Loops : When you want to run an action for certain number of times, we use loops.
+
+# There are 2 major types of loops ; 
+#     1) For loop       ( When you know something to be executed n number of time, we use for loop)
+#     2) While loop     ( When you don't know something to be executed n number of times, we use while loop)
+
+# echo Value is 10
+# echo Value is 20 
+# echo Value is 30
+# echo Value is 40
+# echo Value is 50 
+
+for i in 10 20 30 40 50 ; do 
+echo -e "\n \e[35m \033[1m \t Values from the loop are $i \033[0m \e[0m"done 
