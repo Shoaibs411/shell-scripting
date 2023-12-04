857d9160abf5291ebe8233cebbbe7b31ba3b2b8e
Shoaib <shoaibs411@gmail.com>
2023-12-05 02:02:37 +0530

    Auto Push

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 bash component/$1.sh
+
+if [ $? -ne 0 ]; then
+    echo -e "\n|--------------------------------------------------------------------|"
+    echo -e "\n \e[35m \033[1m \t Example usage : wrapper.sh componentName \033[0m \e[0m"
+    echo -e "\n|--------------------------------------------------------------------|"
+    exit 30
+fi
