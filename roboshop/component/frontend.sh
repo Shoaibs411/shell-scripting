3e902160d9afdeb49beef0c286d672205588fc6c
Shoaib <shoaibs411@gmail.com>
2023-12-05 02:59:38 +0530

    Auto Push

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -14,9 +14,9 @@ echo -e -n "\n Installing nginx :"
 
 yum install nginx -y    &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then
-    echo -e "\e[32m Successfully Installed \e[0m"
+    echo -e "\e[32m Success \e[0m"
 else    
-    echo -e "\e[31m Installation Failed \e[0m"
+    echo -e "\e[31m Failed \e[0m"
 fi
 
 echo -e -n "\n Downloading the component $1 :"
@@ -24,7 +24,7 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
 else    
-    echo -e "\e[31m Failure \e[0m"
+    echo -e "\e[31m Failed \e[0m"
 fi
 
 # systemctl enable nginx
