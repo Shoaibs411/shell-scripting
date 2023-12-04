dc12c610305e5512f423f695ea1f9c8b3c2f2b87
Shoaib <shoaibs411@gmail.com>
2023-12-04 23:33:28 +0530

    Auto Push

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+<<COMMENT
+example of multi line commend
 
 # exploring conditions in shell
+COMMENT
+
 echo -e "\n|--------------------------------------------------------------------|"
 echo -e "\n\t\e[35m \033[1m \033[3m Exploring Conditions in Shell Scripting \033[0m \033[0m \e[0m"
+
+
+
+
+
 
 
 echo -e "\n|--------------------------------------------------------------------|"
