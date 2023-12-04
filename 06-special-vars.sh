57fb6be63d1a2925148a3fedd11af51c7744d02c
Shoaib <shoaibs411@gmail.com>
2023-12-04 19:33:23 +0530

    Auto Push

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -3,7 +3,7 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-echo -e "\n \e[33m "${bold}" Exploring Special variables in Shell Scripting \e[0m "${normal}""
+echo -e "\n \e[33m \033[3m "${bold}" Exploring Special variables in Shell Scripting \033[0m \e[0m "${normal}""
 
 echo -e "\n" '$0' "Prints Script Name : \e[34m $0 \e[0m"
 echo -e "\n" '$#' "Prints the overall arguments used in the script : \e[34m $# \e[0m"
@@ -11,7 +11,7 @@ echo -e "\n" '$?' "prints the exit code of the last command : \e[34m $? \e[0m"
 echo -e "\n" '$*' "Prints all the arguments used. : \e[34m $* \e[0m"
 echo -e "\n" '$@' "Prints all the arguments used. : \e[34m $@ \e[0m"
 
-echo -e '\033[3mSample\033[0m'
+# echo -e '\033[3mSample\033[0m' - sample for priting text in formats. Eg. Bold IOtalic etc.
 
 # echo "Variables Used In The Script $*"    # $* is going to print the used variables  
 # echo "Variabels used are $@"              # $@ is going to print the used variables  
