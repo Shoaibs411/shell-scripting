98141b3f232a196cafaf1827a96ddd2e26b32abf
Shoaib <shoaibs411@gmail.com>
2023-12-04 23:36:35 +0530

    Auto Push

---
+++ 12-redirectors.sh
@@ -0,0 +1,10 @@
+$!/bin/bash
+
+# Explroing redirectors in Shell
+
+echo -e "\n|--------------------------------------------------------------------|"
+echo -e "\n\t\e[35m \033[1m \033[3m Exploring Redirectos in Shell Scripting \033[0m \033[0m \e[0m"
+
+
+
+echo -e "\n|--------------------------------------------------------------------|"
