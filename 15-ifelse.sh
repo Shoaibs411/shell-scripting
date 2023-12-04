24362206e5b34f510c7cff007cb9a76ad2102a41
Shoaib <shoaibs411@gmail.com>
2023-12-05 00:44:50 +0530

    Auto Push

--- 15-ifelse.sh
+++ 15-ifelse.sh
@@ -5,7 +5,6 @@
 
 echo -e "\n|--------------------------------------------------------------------|"
 echo -e "\n \e[35m \033[1m \t Exploring If, If Else & Else If in Shell Scripting \033[0m \e[0m"
-echo -e "\n|--------------------------------------------------------------------|"
 
 ACTION=$1
 
@@ -14,3 +13,16 @@ if [ "$ACTION" == "start" ] ; then
         echo -e "\n|--------------------------------------------------------------------|"
         exit 0
 fi
+
+if [ "$ACTION" == "stop" ] ; then
+        echo -e "\n \e[32m \033[1m \033[3m \t Starting Shipping Services\033[0m \033[0m \e[0m"
+        echo -e "\n|--------------------------------------------------------------------|"
+        exit 1
+fi
+
+if [ "$ACTION" == "restart" ] ; then
+        echo -e "\n \e[32m \033[1m \033[3m \t Starting Shipping Services\033[0m \033[0m \e[0m"
+        echo -e "\n|--------------------------------------------------------------------|"
+        exit 2
+fi
+
