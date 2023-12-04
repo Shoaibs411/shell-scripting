a10540a3add05434d2489caf1cdbe8e53d4b9d11
Shoaib <shoaibs411@gmail.com>
2023-12-05 00:45:53 +0530

    Auto Push

--- 15-ifelse.sh
+++ 15-ifelse.sh
@@ -15,13 +15,13 @@ if [ "$ACTION" == "start" ] ; then
 fi
 
 if [ "$ACTION" == "stop" ] ; then
-        echo -e "\n \e[32m \033[1m \033[3m \t Starting Shipping Services\033[0m \033[0m \e[0m"
+        echo -e "\n \e[31m \033[1m \033[3m \t Stopping Shipping Services\033[0m \033[0m \e[0m"
         echo -e "\n|--------------------------------------------------------------------|"
         exit 1
 fi
 
 if [ "$ACTION" == "restart" ] ; then
-        echo -e "\n \e[32m \033[1m \033[3m \t Starting Shipping Services\033[0m \033[0m \e[0m"
+        echo -e "\n \e[33m \033[1m \033[3m \t Restarting Shipping Services\033[0m \033[0m \e[0m"
         echo -e "\n|--------------------------------------------------------------------|"
         exit 2
 fi
