e43fb2564ee618a23b6beec2e32f21c8cc88faa0
Shoaib <shoaibs411@gmail.com>
2023-12-05 02:36:52 +0530

    Auto Push

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -2,9 +2,11 @@
 
 bash component/$1.sh
 
+<<comment
 if [ $? -ne 0 ] ; then
     echo -e "\n|--------------------------------------------------------------------|"
     echo -e "\n \e[31m \033[1m \t Example usage : wrapper.sh componentName \033[0m \e[0m"
     echo -e "\n|--------------------------------------------------------------------|"
     exit 30
 fi
+comment
