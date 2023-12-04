3c7038a3155bb7df4b3cc014919b09fb62098e40
Shoaib <shoaibs411@gmail.com>
2023-12-05 00:08:56 +0530

    Auto Push

--- 11-exit.sh
+++ 11-exit.sh
@@ -2,16 +2,11 @@
 <<COMMENT
 example of multi line commend
 
-# exploring conditions in shell
+# exploring multi line comment in shell
 COMMENT
 
 echo -e "\n|--------------------------------------------------------------------|"
-echo -e "\n\t\e[35m \033[1m \033[3m Exploring Conditions in Shell Scripting \033[0m \033[0m \e[0m"
-
-
-
-
-
+echo -e "\n\t\e[35m \033[1m \033[3m Exploring Multi line comment in Shell Scripting \033[0m \033[0m \e[0m"
 
 
 echo -e "\n|--------------------------------------------------------------------|"
