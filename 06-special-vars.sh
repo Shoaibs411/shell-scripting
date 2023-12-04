32b814535669b3bd1d66fb8dbea0ddbc52ccca5c
Shoaib <shoaibs411@gmail.com>
2023-12-04 19:07:28 +0530

    Auto Push

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo -e "\n \e[33m Exploring special variables in Shell Scripting \e[0m"
+echo -e "\n \e[33m Exploring Special variables in Shell Scripting \e[0m"
+
+echo -e "\nPrinting the Script Name : \e [34m $0 \e [0m"
