4992e77e648e5ec104912e84e5c58092faec034f
Shoaib <shoaibs411@gmail.com>
2023-12-04 23:27:44 +0530

    Auto Push

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # exploring conditions in shell
-echo "--------------------------------------------------------------------"
+echo -e"\n--------------------------------------------------------------------"
 echo -e "\n \e[35m \033[1m \033[3m "${bold}" Exploring Conditions in Shell Scripting \033[0m \033[0m \e[0m "${normal}""
 
 
