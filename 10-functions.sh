cb247482797c566d90235cda4f1bd39a4172b5c6
Shoaib <shoaibs411@gmail.com>
2023-12-04 19:55:56 +0530

    Auto Push

--- 10-functions.sh
+++ 10-functions.sh
@@ -15,8 +15,19 @@ echo -e "\n \e[35m \033[1m \033[3m "${bold}" Exploring Funtions in Shell Scripti
 
 b56() {
 
-    echo -e "\n This a function b56"
+    echo -e "\n This is inside a function b56"
 }
 
 b56
 echo -e "\n Called out the function b56" 
+
+stat() {
+
+    echo "Number of sessions opened $(who|wc -l)"
+    echo "Todays date is $(date +%F)"
+    echo "AVG Cpu Utilization In The Last 5 minues is : $(uptime|awk -F : '{print $NF}' | awk -F ',' '{print $2}')"
+
+    b56  # calling a function from another function
+}
+
+stat 
