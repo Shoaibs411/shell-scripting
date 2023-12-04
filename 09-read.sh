eae52afb2cc9a59c12dedf0616286d17824076e6
Shoaib <shoaibs411@gmail.com>
2023-12-04 19:44:40 +0530

    Auto Push

--- 09-read.sh
+++ 09-read.sh
@@ -2,5 +2,5 @@
 
 # Taking inputs from command line
 
-read -p "Enter your name :" name
+read -p -e "\n Enter your name :" name
 echo "Name of the user is $name"
---
+++ 10-functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Exploring Functions in Bash
+echo -e "\n \e[33m \033[3m "${bold}" Exploring Funtions in Shell Scripting \033[0m \e[0m "${normal}""
+
+# There are 4 types of command available : 
+
+# 1) Binary                   ( /bin  , /sbin )
+# 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn" )
+# 3) Shell Built-in Commands  
+# 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per your choice.
