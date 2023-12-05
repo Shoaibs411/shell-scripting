5bee92746f197d4be7a6d35f6e04f564c35028c1
Shoaib <shoaibs411@gmail.com>
2023-12-06 03:57:59 +0530

    Auto Push

--- roboshop/component/user.sh
+++ roboshop/component/user.sh
@@ -5,6 +5,8 @@ echo -e "\n|--------------------------------------------------------------|"
 echo -e "\n\t   ********** \e[35m \033[1m Configuring ${COMPONENT} \033[0m \e[0m **********"
 
 source component/common.sh
-    
+
+NODEJS      # Calling the NodeJS function to install & configure NodeJS
+
 echo -e -n "\n     ********** \e[35m \033[1m ${COMPONENT} Configuration Completed \033[0m \e[0m **********"
 echo -e -n "\n|--------------------------------------------------------------|\n"
