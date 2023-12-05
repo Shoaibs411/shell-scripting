d1b857f8f4b702657134aaf3ad3e81a05c501877
Shoaib <shoaibs411@gmail.com>
2023-12-06 02:27:31 +0530

    Auto Push

--- roboshop/component/redis.sh
+++ roboshop/component/redis.sh
@@ -16,8 +16,8 @@ if [ $UID -ne 0 ]; then
     echo -e "\n\t\e[35m Example Usage : sudo bash scriptName componentName \e[0m"
     exit 1
 fi
-echo -e -n "\n|--------------------------------------------------------------|"
-echo -e -n "\n\t   ********** \e[35m \033[1m Configuring ${COMPONENT} \033[0m \e[0m **********"
+echo -e "\n|--------------------------------------------------------------|"
+echo -e "\n\t   ********** \e[35m \033[1m Configuring ${COMPONENT} \033[0m \e[0m **********"
 
 echo -n "Configuring Repositories :"
 curl -L https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo -o /etc/yum.repos.d/${COMPONENT}.repo     &>> $LOGFILE  
