7d637b76d1a3b870709aef9a622a209cea40e17a
Shoaib <shoaibs411@gmail.com>
2023-12-05 23:42:43 +0530

    Auto Push

--- roboshop/component/mongo.sh
+++ roboshop/component/mongo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USERID=$(id -u)
-COMPONENT=mongo
+COMPONENT=mongodb
 LOGFILE="/tmp/${COMPONENT}.log"
 MONGO_REPO="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
 SCHEMA_URL="https://github.com/stans-robot-project/mongodb/archive/main.zip"
