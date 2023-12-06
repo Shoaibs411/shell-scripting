c586bbca14c449e46eea7c2942b10bfc47377511
Shoaib <shoaibs411@gmail.com>
2023-12-06 19:17:19 +0530

    Auto Push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -19,5 +19,14 @@ systemctl enable mysqld
 systemctl start mysqld
 status $?
 
+echo -n "Extracting ${COMPONENT} Default root Password :"
+DEFAULT_ROOT_PASS=$(sudo grep "temporary password" /var/log/mysqld.log | awk -F " " '{print $NF}')
+echo -n "Printing Default root pw : " $DEFAULT_ROOT_PASS
+status $?
+
+
+
+
 echo -e -n "\n     ********** \e[35m \033[1m ${COMPONENT} Configuration Completed \033[0m \e[0m **********"
 echo -e -n "\n|--------------------------------------------------------------|\n"
+
