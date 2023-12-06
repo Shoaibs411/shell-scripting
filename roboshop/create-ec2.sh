780272215b3c1bf77cdadd5403ebbc52dd31e55e
Shoaib <shoaibs411@gmail.com>
2023-12-07 03:15:36 +0530

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -1,17 +1,22 @@
-#!/bin/bash
+#!/bin/bash 
 
-# This script is going create EC2 servers on AWS.
-COMPONENT=$1
-HOSTEDZONEID="Z08445993T3OQ7PISU2JV"
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
-SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-allow-all" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
-INSTANCE_TYPE="t2.micro"
+# This script is going to create EC2 Servers
+
+# AMI_ID="ami-0f75a13ad2e340a58" # Hardcoding is a bad-choice particularly with AMI_ID as it's going be changed when you register a new AMI.
+# SGID="sg-052fd946b7e11841a"
 
 if [ -z $1 ] || [ -z $2 ] ; then 
     echo -e "\e[31m ****** COMPONENT NAME & ENV ARE NEEDED ****** \e[0m \n\t\t"
     echo -e "\e[36m \t\t Example Usage : \e[0m  bash create-ec2 ratings dev"
     exit 1 
 fi 
+
+COMPONENT=$1
+ENV=$2
+HOSTEDZONEID="Z08445993T3OQ7PISU2JV"
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-allow-all" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
+INSTANCE_TYPE="t3.micro"
 
 create_server() {
     echo -e "******* \e[32m $COMPONENT-$ENV \e[0m Server Creation In Progress ******* !!!!!!"
@@ -24,7 +29,7 @@ create_server() {
 
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json\
 
-    echo -e "******* \e[32m $COMPONENT-$ENV \e[0m DNS Record Creation Is Completed ******* !!!!!! \n\n"
+    echo -e "******* \e[32m $COMPONENT-$ENV \e[0m DNS Record Creation Is Complted ******* !!!!!! \n\n"
 }
 
 # If the user supplies all as the first argument, then all these servers will be created.
