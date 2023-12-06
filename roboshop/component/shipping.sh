4b61c73dd7d1835c6ccd18715919f15e44ee1a64
Shoaib <shoaibs411@gmail.com>
2023-12-06 22:03:57 +0530

    Auto Push

--- roboshop/component/shipping.sh
+++ roboshop/component/shipping.sh
@@ -3,11 +3,11 @@ COMPONENT=shipping
 
 
 echo -e "\n|--------------------------------------------------------------|"
-echo -e "\n\t ********** \e[35m \033[1m Configuring ${COMPONENT} \033[0m \e[0m **********\n"
+echo -e "\n\t ********** \e[35m \033[1m Configuring ${COMPONENT} \033[0m \e[0m **********"
 
 source component/common.sh
     
 MAVEN      # Calling the MAVEN function to install & configure JAVA
 
 echo -e -n "\n   ********** \e[35m \033[1m ${COMPONENT} Configuration Completed \033[0m \e[0m **********"
-echo -e -n "\n|--------------------------------------------------------------|\n"
+echo -e "\n|--------------------------------------------------------------|\n"
