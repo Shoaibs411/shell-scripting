54c67cde705128ac0ade22b07b3b335a175e4455
Shoaib <shoaibs411@gmail.com>
2023-12-06 19:06:57 +0530

    Auto Push

--- roboshop/component/mysql.sh
+++ roboshop/component/mysql.sh
@@ -7,17 +7,17 @@ echo -e "\n|--------------------------------------------------------------|"
 echo -e "\n\t   ********** \e[35m \033[1m Configuring ${COMPONENT} \033[0m \e[0m **********"
 
 echo -n "Configuring ${COMPONENT} repo :"
-    curl -s -L -o /etc/yum.repos.d/mysql.repo $COMPONENT_URL
-    status $?
+curl -s -L -o /etc/yum.repos.d/mysql.repo $COMPONENT_URL
+status $?
 
 echo -n "Installing ${COMPONENT} :"
-    yum install mysql-community-server -y &>> $LOGFILE
-    status $?
+yum install mysql-community-server -y &>> $LOGFILE
+status $?
 
 echo -n "Starting ${COMPONENT} :"
-    systemctl enable mysqld 
-    systemctl start mysqld
-    status $?
+systemctl enable mysqld 
+systemctl start mysqld
+status $?
 
 echo -e -n "\n     ********** \e[35m \033[1m ${COMPONENT} Configuration Completed \033[0m \e[0m **********"
 echo -e -n "\n|--------------------------------------------------------------|\n"
