b1df30e51724e398034e399db288649a838d4905
Shoaib <shoaibs411@gmail.com>
2023-12-07 02:30:35 +0530

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -8,12 +8,14 @@ SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-al
 INSTANCE_TYPE="t2.micro"
 
     
+echo -e "******* \e[32m $COMPONENT \e[0m Server Creation In Progress ******* !!!!!!"
+
+    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
     echo -e "******* \e[32m $COMPONENT \e[0m Server Creation Is Complted ******* !!!!!! \n\n"
-    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
 
     echo -e "******* \e[32m $COMPONENT \e[0m DNS Record Creation In Progress ******* !!!!!!"
     sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
 
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json\
 
-    echo -e "******* \e[32m $COMPONENT-$ENV \e[0m DNS Record Creation Is Complted ******* !!!!!! \n\n"
+    echo -e "******* \e[32m $COMPONENT \e[0m DNS Record Creation Is Complted ******* !!!!!! \n\n"
