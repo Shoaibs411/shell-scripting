87f173fab8630d66947d1609e85d36cb714ede1d
Shoaib <shoaibs411@gmail.com>
2023-12-07 02:21:27 +0530

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -4,7 +4,7 @@
 COMPONENT=$1
 HOSTEDZONEID="Z08445993T3OQ7PISU2JV"
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
-SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=Linux Training" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-allow-all" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 INSTANCE_TYPE="t2.micro"
 
 echo -e "\n|--------------------------------------------------------------|"
